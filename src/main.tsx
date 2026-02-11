8b791aa6aa1f0a000d044f47ea8e20ee8e088f8f
Mariana Loaiza <marianaloaiza200501@gmail.com>
2026-02-11 11:05:05 -0400

    Instalaciones y configuraciones: tailwindcss y shandcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { Button } from './components/ui/button'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <h1>Hola mundo</h1>
+    <Button>
+      Hola de nuevo
+    </Button>
   </StrictMode>,
 )
