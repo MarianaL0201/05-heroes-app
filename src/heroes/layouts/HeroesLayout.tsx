c5bb0320639078e9369956a0588aadd0f3e6aa45
Mariana Loaiza <marianaloaiza200501@gmail.com>
2026-02-18 20:54:20 -0400

    Arreglo de pading en toda la pagina

--- src/heroes/layouts/HeroesLayout.tsx
+++ src/heroes/layouts/HeroesLayout.tsx
@@ -4,10 +4,12 @@ import { Outlet } from "react-router"
 export const HeroesLayout = () => {
   return (
         <div className="min-h-screen bg-linear-to-br from-slate-50 via-blue-50 to-purple-50">
-        <div className="max-w-7xl mx-auto p-6"></div>
+        <div className="max-w-7xl mx-auto p-6">
+
           <CustomMenu/>
 
           <Outlet/>
+        </div>
     </div>
   )
 }
