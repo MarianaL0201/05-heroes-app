a31c3d6a3fecc32986566d826653db2549efcc83
Mariana Loaiza <marianaloaiza200501@gmail.com>
2026-03-05 10:21:38 -0400

    Agregamos search-heroes.actions.ts: funcionamiento de la barra de busqueda

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,9 +1,24 @@
+import { useQuery } from "@tanstack/react-query"
 import { CustomJumbotron } from "@/components/Custom/CustomJumbotron"
 import { HeroStats } from "@/heroes/components/HeroStats"
 import { SearchControls } from "./ui/SearchControls"
 import { CustomBreadcrumbs } from "@/components/Custom/CustomBreadcrumbs"
+import { HeroGrid } from "@/heroes/components/HeroGrid"
+import { useSearchParams } from "react-router"
+import { searchHeroesAction } from "@/heroes/actions/search-heroes.actions"
 
 export const SearchPage = () => {
+
+  const [searchParams] = useSearchParams();
+
+  const name = searchParams.get('name') ?? undefined;
+
+  const {data: heroes = []} = useQuery({
+    queryKey: ['search', {name: name}],
+    queryFn: () => searchHeroesAction({name}),
+    staleTime: 1000 * 60 * 5,
+  });
+
   return (
     <>
       <CustomJumbotron
@@ -26,6 +41,10 @@ export const SearchPage = () => {
 
       {/* Filter and Search */}
       <SearchControls />
+
+      <HeroGrid heroes={heroes}/>
     </>
   )
 }
+
+export default SearchPage;
